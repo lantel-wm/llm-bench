208c51ebb196a0aca49cd09312c9cb3492707619
Zhiyu Zhao <zhaozhiyu@sensetime.com>
2024-06-05 16:32:33 +0800

    benchmark_all_cuda

---
+++ benchmark_all_cuda.sh
@@ -0,0 +1,139 @@
+SCRIPT=$(realpath -s "$0")
+PERF_BASE_PATH=$(dirname "$SCRIPT")
+
+function unittest() {
+    MODEL_SIZE=$1
+    GPUS=$2
+    BATCH=$3
+    INLEN=$4
+    OUTLEN=$5
+    MODE=$6
+    echo "[BENCHMARK ${MODEL_SIZE}B TP${GPUS} BATCH${BATCH} I${INLEN} O${OUTLEN} ${MODE^^}]"
+    LATENCY=`bash $PERF_BASE_PATH/benchmark_one_cuda_${MODE}.sh ${MODEL_SIZE} ${GPUS} ${BATCH} ${INLEN} ${OUTLEN} | grep -oP "Avg latency: \K[0-9]+\.[0-9]+"`
+
+    if [ ! -n "$LATENCY" ]; then
+        echo "[FAILED]"
+    else
+        LATENCY=$(echo "scale=2; $LATENCY * 1000" | bc)
+        O_TPS=$(echo "scale=4; $BATCH * $OUTLEN / ($LATENCY / 1000)" | bc)
+        IO_TPS=$(echo "scale=4; $BATCH * ($OUTLEN + $INLEN) / ($LATENCY / 1000)" | bc)
+        echo "[OK] $LATENCY"
+        echo "$MODEL_SIZE,$GPUS,$BATCH,$INLEN,$OUTLEN,$MODE,$LATENCY,$O_TPS,$IO_TPS" >> $PERF_BASE_PATH/benchmark_all_cuda_result.csv        
+    fi
+}
+
+echo "model_size(B),tp,batch,inlen,outlen,mode,latency(ms),o_tps,io_tps" > $PERF_BASE_PATH/benchmark_all_cuda_result.csv
+
+_MODE_LIST=(fp16)
+
+_I8O256_BATCH_SIZE_LIST=(1 2 4 8 16 32 48 64 80 96 112 128 160 192 224 256 384 512 768 1024 1280 1536 1792 2048)
+_I128O128_BATCH_SIZE_LIST=(1 2 4 8 16 32 48 64 80 96 112 128 160 192 224 256 384 512 768 1024 1280 1536 1792 2048)
+_I2048O2048_BATCH_SIZE_LIST=(1 2 4 8 16 32 48 64 80 96 112 128 160 192 224 256)
+_I128O2048_BATCH_SIZE_LIST=(1 2 4 8 16 32 48 64 80 96 112 128 160 192 224 256)
+_I2048O128_BATCH_SIZE_LIST=(1 2 4 8 16 32 48 64 80 96 112 128 160 192 224 256)
+_I1024O1024_BATCH_SIZE_LIST=(1 2 4 8 16 32 48 64 80 96 112 128 160 192 224 256)
+_I1024O512_BATCH_SIZE_LIST=(1 2 4 8 16 32 48 64 80 96 112 128 160 192 224 256)
+_I512O512_BATCH_SIZE_LIST=(1 2 4 8 16 32 48 64 80 96 112 128 160 192 224 256 384 512)
+_I256O512_BATCH_SIZE_LIST=(1 2 4 8 16 32 48 64 80 96 112 128 160 192 224 256 384 512 768 1024)
+_I128KO128_BATCH_SIZE_LIST=(1 2 4)
+_I64KO128_BATCH_SIZE_LIST=(1 2 4 8)
+_I32KO128_BATCH_SIZE_LIST=(1 2 4 8 12 16)
+_I16KO128_BATCH_SIZE_LIST=(1 2 4 8 12 16 20 24 28 32)
+
+
+for MODE in ${_MODE_LIST[@]}; do
+
+
+for BATCH_SIZE in ${_I8O256_BATCH_SIZE_LIST[@]}; do
+    unittest 7 1 $BATCH_SIZE 8 256 $MODE
+done
+for BATCH_SIZE in ${_I256O512_BATCH_SIZE_LIST[@]}; do
+    unittest 7 1 $BATCH_SIZE 256 512 $MODE
+done
+for BATCH_SIZE in ${_I512O512_BATCH_SIZE_LIST[@]}; do
+    unittest 7 1 $BATCH_SIZE 512 512 $MODE
+done
+for BATCH_SIZE in ${_I1024O512_BATCH_SIZE_LIST[@]}; do
+    unittest 7 1 $BATCH_SIZE 1024 512 $MODE
+done
+for BATCH_SIZE in ${_I1024O1024_BATCH_SIZE_LIST[@]}; do
+    unittest 7 1 $BATCH_SIZE 1024 1024 $MODE
+done
+for BATCH_SIZE in ${_I16KO128_BATCH_SIZE_LIST[@]}; do
+    unittest 7 1 $BATCH_SIZE 16384 128 $MODE
+done
+for BATCH_SIZE in ${_I32KO128_BATCH_SIZE_LIST[@]}; do
+    unittest 7 1 $BATCH_SIZE 32768 128 $MODE
+done
+
+
+for BATCH_SIZE in ${_I8O256_BATCH_SIZE_LIST[@]}; do
+    unittest 13 2 $BATCH_SIZE 8 256 $MODE
+done
+for BATCH_SIZE in ${_I256O512_BATCH_SIZE_LIST[@]}; do
+    unittest 13 2 $BATCH_SIZE 256 512 $MODE
+done
+for BATCH_SIZE in ${_I512O512_BATCH_SIZE_LIST[@]}; do
+    unittest 13 2 $BATCH_SIZE 512 512 $MODE
+done
+for BATCH_SIZE in ${_I1024O512_BATCH_SIZE_LIST[@]}; do
+    unittest 13 2 $BATCH_SIZE 1024 512 $MODE
+done
+for BATCH_SIZE in ${_I1024O1024_BATCH_SIZE_LIST[@]}; do
+    unittest 13 2 $BATCH_SIZE 1024 1024 $MODE
+done
+for BATCH_SIZE in ${_I16KO128_BATCH_SIZE_LIST[@]}; do
+    unittest 13 2 $BATCH_SIZE 16384 128 $MODE
+done
+for BATCH_SIZE in ${_I32KO128_BATCH_SIZE_LIST[@]}; do
+    unittest 13 2 $BATCH_SIZE 32768 128 $MODE
+done
+
+
+for BATCH_SIZE in ${_I8O256_BATCH_SIZE_LIST[@]}; do
+    unittest 65 8 $BATCH_SIZE 8 256 $MODE
+done
+for BATCH_SIZE in ${_I256O512_BATCH_SIZE_LIST[@]}; do
+    unittest 65 8 $BATCH_SIZE 256 512 $MODE
+done
+for BATCH_SIZE in ${_I512O512_BATCH_SIZE_LIST[@]}; do
+    unittest 65 8 $BATCH_SIZE 512 512 $MODE
+done
+for BATCH_SIZE in ${_I1024O512_BATCH_SIZE_LIST[@]}; do
+    unittest 65 8 $BATCH_SIZE 1024 512 $MODE
+done
+for BATCH_SIZE in ${_I1024O1024_BATCH_SIZE_LIST[@]}; do
+    unittest 65 8 $BATCH_SIZE 1024 1024 $MODE
+done
+for BATCH_SIZE in ${_I16KO128_BATCH_SIZE_LIST[@]}; do
+    unittest 65 8 $BATCH_SIZE 16384 128 $MODE
+done
+for BATCH_SIZE in ${_I32KO128_BATCH_SIZE_LIST[@]}; do
+    unittest 65 8 $BATCH_SIZE 32768 128 $MODE
+done
+
+
+for BATCH_SIZE in ${_I8O256_BATCH_SIZE_LIST[@]}; do
+    unittest 70 8 $BATCH_SIZE 8 256 $MODE
+done
+for BATCH_SIZE in ${_I256O512_BATCH_SIZE_LIST[@]}; do
+    unittest 70 8 $BATCH_SIZE 256 512 $MODE
+done
+for BATCH_SIZE in ${_I512O512_BATCH_SIZE_LIST[@]}; do
+    unittest 70 8 $BATCH_SIZE 512 512 $MODE
+done
+for BATCH_SIZE in ${_I1024O512_BATCH_SIZE_LIST[@]}; do
+    unittest 70 8 $BATCH_SIZE 1024 512 $MODE
+done
+for BATCH_SIZE in ${_I1024O1024_BATCH_SIZE_LIST[@]}; do
+    unittest 70 8 $BATCH_SIZE 1024 1024 $MODE
+done
+for BATCH_SIZE in ${_I16KO128_BATCH_SIZE_LIST[@]}; do
+    unittest 70 8 $BATCH_SIZE 16384 128 $MODE
+done
+for BATCH_SIZE in ${_I32KO128_BATCH_SIZE_LIST[@]}; do
+    unittest 70 8 $BATCH_SIZE 32768 128 $MODE
+done
+
+
+done
---
+++ benchmark_templ_cuda.sh
@@ -0,0 +1,57 @@
+SCRIPT=$(realpath -s "$0")
+PERF_BASE_PATH=$(dirname "$SCRIPT")
+
+if [ ! -n "$BENCHMARK_LLM" ]; then
+    echo "[ERROR] please set env BENCHMARK_LLM to the benchmark_llama executable"
+    exit 1
+fi
+
+MODEL_SIZE=$1
+
+if [ ! -n "$MODEL_SIZE" ]; then
+    MODEL_SIZE=7
+fi
+
+TP_SIZE=$2
+
+if [ ! -n "$TP_SIZE" ]; then
+    TP_SIZE=1
+fi
+
+BATCH=$3
+
+if [ ! -n "$BATCH" ]; then
+    BATCH=1
+fi
+
+IN_LEN=$4
+
+if [ ! -n "$IN_LEN" ]; then
+    IN_LEN=8
+fi
+
+OUT_LEN=$5
+
+if [ ! -n "$OUT_LEN" ]; then
+    OUT_LEN=256
+fi
+
+MODEL_DIR="$PERF_BASE_PATH/../hf_models/llama-${MODEL_SIZE}b-hf"
+# MODEL_PARAM_PATH="$PERF_BASE_PATH/../hf_models/llama_${MODEL_SIZE}b_${TP_SIZE}gpu/params.json"
+WARMUP_LOOPS=2
+BENCHMARK_LOOPS=2
+
+CMD="python ${BENCHMARK_LLM} \
+--model $MODEL_DIR \
+--tensor-parallel-size $TP_SIZE \
+--num-iters-warmup $WARMUP_LOOPS \
+--num-iters $BENCHMARK_LOOPS \
+--input-len $IN_LEN \
+--output-len $OUT_LEN \
+--batch-size $BATCH \
+$BENCHMARK_EXTENDED_OPTIONS"
+
+echo "BENCH MODEL${MODEL_SIZE}B TP${TP_SIZE} BATCH${BATCH} I${IN_LEN}O${OUT_LEN} -> $CMD"
+
+eval "$CMD"
+
