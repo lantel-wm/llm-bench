99b9269256fbba19f802fb7da7d3e65ed1981981
Zhiyu Zhao <63342039+lantel-wm@users.noreply.github.com>
2024-06-07 11:17:15 +0800

    Add build.sh (#2)
    * before rebase
    
    * .gitignore
    
    * start writing shell scripts
    
    * benchmark_all_cuda
    
    * .gitignore
    
    * README.md
    
    * build.sh
    
    * README.md
    
    * before rebase
    
    * pr
    
    ---------
    
    Co-authored-by: Zhiyu Zhao <zhaozhiyu@sensetime.com>

--- README.md
+++ README.md
@@ -1,19 +1,38 @@
 # vllm-static-bench
 
-## 调整测试算例
+## 前置准备
 
-`benchmark_all_cuda.sh`中可以调整测试的batch size，model size，tp size，in len，out len，mode等参数。
+1.conda环境：
+
+新建一个conda环境，使用python3.9版本，不要用已有的conda环境进行测试。
+
+```shell
+conda create -n vllm_static_bench python=3.9 -y
+conda activate vllm_static_bench
+```
+
+2.安装依赖：
+
+根据本机CUDA版本进行安装，支持CUDA11和CUDA12，推荐使用CUDA12。
+
+```shell
+## CUDA 12.x
+bash ./build.sh 12
+
+## CUDA 11.x
+bash ./build.sh 11
+```
 
 ## 测试启动
 
-运行脚本之前，先设置BENCHMARK_LLM环境变量：
+运行脚本之前，先设置`BENCHMARK_LLM`环境变量：
 
 ```shell
-# vllm==0.4.3
-export BENCHMARK_LLM=${pwd}/python/benchmark_latency.py
+# CUDA 12.x, vllm==0.4.3
+export BENCHMARK_LLM="$(dirname "$(realpath ".")")/python/benchmark_latency.py"
 
-# vllm<=0.4.2
-export BENCHMARK_LLM=${pwd}/python/benchmark_latency_old_version.py
+# CUDA 11.x, vllm<=0.4.2
+export BENCHMARK_LLM="$(dirname "$(realpath ".")")/python/benchmark_latency_old_version.py"
 ```
 
 启动`benchmark_all_cuda.sh`即可开始测试：
@@ -26,6 +45,10 @@ bash benchmark_all_cuda.sh
 ```shell
 bash benmark_one_cuda_fp16.sh
 ```
+
+## 调整测试算例
+
+`benchmark_all_cuda.sh`中可以调整测试的batch size，model size，tp size，in len，out len，mode等参数。
 
 ## 结果保存
 
---
+++ build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+# 帮助信息函数
+show_help() {
+  echo "Usage: $0 <cuda_version>"
+  echo ""
+  echo "Install the appropriate version of vllm based on the specified CUDA version."
+  echo ""
+  echo "Arguments:"
+  echo "  -h                Show this help message and exit."
+  echo "  <cuda_version>    The CUDA version to use for the installation (either 11 or 12)."
+  echo ""
+  echo "Examples:"
+  echo "  $0 11             Install vllm for CUDA 11."
+  echo "  $0 12             Install vllm for CUDA 12."
+  echo ""
+  echo "Script absolute path: $SCRIPT_DIR"
+}
+
+# 检查输入参数
+if [ $# -ne 1 ]; then
+  show_help
+  exit 1
+fi
+
+# 获取输入参数
+PARAM=$1
+
+# 检查是否需要显示帮助信息
+if [ "$PARAM" == "-h" ]; then
+  show_help
+  exit 0
+fi
+
+# 警告信息并询问用户确认
+echo -e "[WARNING] This script should be run in a newly created conda environment."
+read -p "Are you sure to continue? (Y/n): " confirm
+
+if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
+  echo "[INFO] Installation aborted."
+  exit 1
+fi
+
+# 安装不同版本的vllm
+case $PARAM in
+  "11")
+    BENCHMARK_LLM="$SCRIPT_DIR/python/benchmark_latency_old_version.py"
+    echo "[INFO] Installing vllm==0.4.2 for CUDA 11..."
+    pip install -r requirements_vllm042.txt
+    ;;
+  "12")
+    BENCHMARK_LLM="$SCRIPT_DIR/python/benchmark_latency.py"
+    echo "[INFO] Installing vllm==0.4.3 for CUDA 12..."
+    pip install -r requirements_vllm043.txt
+    ;;
+  *)
+    echo "[ERROR] Unsupported CUDA version: $PARAM. Only 11 and 12 are supported."
+    exit 1
+    ;;
+esac
+
+echo "[INFO] vllm installed for CUDA $PARAM"
