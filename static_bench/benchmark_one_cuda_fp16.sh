b2d436516df42bf48fd0f316a9d8c4b8d728c4d4
Zhiyu Zhao <zhaozhiyu@sensetime.com>
2024-06-25 13:56:57 +0800

    change scripts

--- static_bench/benchmark_one_cuda_fp16.sh
+++ static_bench/benchmark_one_cuda_fp16.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 SCRIPT=$(realpath -s "$0")
 PERF_BASE_PATH=$(dirname "$SCRIPT")
 
-export BENCHMARK_EXTENDED_OPTIONS=
+export BENCHMARK_EXTENDED_OPTIONS="--enforce-eager"
 
-bash $PERF_BASE_PATH/benchmark_templ_cuda.sh $*
+bash "$PERF_BASE_PATH/benchmark_templ_cuda.sh" $*
